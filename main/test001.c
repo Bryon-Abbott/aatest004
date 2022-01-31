95f0b376f4287d6d255a4bc9fadbbe9388985be6
Bryon-Abbott <Bryon.Abbott@gmail.com>
2022-01-31 12:41:07 -0700

    Commit  submodules

---
+++ main/test001.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "led_strip.h"
+#include "sdkconfig.h"
+#include "StateMachine.h"
+#include "Led.h"
+// main function -
+// where the execution of program begins
+int main()
+{
+  
+    // prints hello world
+    printf("Hello Bryon\n");
+  
+    return 0;
+}
